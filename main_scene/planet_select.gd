2dbd8865a8c0d5c46579b8121a34a5158d7eca85
Nont Nonta <nontnta@gmail.com>
2024-02-03 02:42:54 +0700

    3

--- main_scene/planet_select.gd
+++ main_scene/planet_select.gd
@@ -13,6 +13,7 @@ extends Node2D
 
 func _ready():
 	Transition.takeOffArea = true
+	Transition.inMenu = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,42 +22,52 @@ func _process(delta):
 
 
 func _on_mercury_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(mercury, "mercury")
 
 
 func _on_venus_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(venus, "venus")
 
 
 func _on_earth_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(earth, "earth")
 
 
 func _on_mars_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(mars, "mars")
 
 
 func _on_jupiter_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(jupiter, "jupiter")
 
 
 func _on_saturn_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(saturn, "saturn")
 
 
 func _on_uranus_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(uranus, "uranus")
 
 
 func _on_neptune_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(neptune, "neptune")
 
 
 func _on_pluto_pressed():
+	Transition.inMenu = false
 	Transition.change_scene(pluto, "pluto")
 
 func _input(event):
 	if event.is_action_pressed("esc") && Transition.takeOffArea:
+		Transition.inMenu = false
 		match Transition.current_scene:
 			"mercury":
 				Transition.resume(mercury, "mercury")
--- main_scene/transition.gd
+++ main_scene/transition.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 var current_scene
 var takeOffArea = false
+var inMenu = false
 
 func change_scene(planet: PackedScene, name):
 	current_scene = name
@@ -28,6 +29,9 @@ func _process(delta):
 	pass
 
 func _input(event):
-	if event.is_action_pressed("esc") && takeOffArea:
-		$transition/ColorRect.visible = false
+	if event.is_action_pressed("rightClick") && takeOffArea && !inMenu:
+		$transition/ColorRect.visible = true
+		$transition.play("fadeout")
+		await get_tree().create_timer(0.5, false).timeout
 		get_tree().change_scene_to_packed(menu)
+		$transition.play("fadein")
