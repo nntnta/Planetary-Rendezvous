92ec5250b52843e159cfdc22b63f0428cba58b2b
Nont Nonta <nontnta@gmail.com>
2024-02-04 14:20:43 +0700

    4

--- player/player.gd
+++ player/player.gd
@@ -5,4 +5,15 @@ const SPEED = 15000.0
 
 func _physics_process(delta):
 	velocity  = Input.get_vector("a",'d','w','s') * SPEED * delta
-	move_and_slide()
+	print(velocity)
+	if velocity == Vector2.ZERO:
+		$AnimationTree.get("parameters/playback").travel("idle")
+	
+	else:
+		$AnimationTree.get("parameters/playback").travel("walk")
+		$AnimationTree.set("parameters/idle/blend_position", velocity)
+		$AnimationTree.set("parameters/walk/blend_position", velocity)
+		move_and_slide()
+	
+	
+	
