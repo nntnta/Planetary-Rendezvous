6379e7ac1aa1b9f7b859831c50bbe2766ae56bb6
Nont Nonta <nontnta@gmail.com>
2024-02-09 14:45:30 +0700

    9

--- player/player.gd
+++ player/player.gd
@@ -3,9 +3,12 @@ extends CharacterBody2D
 const SPEED = 15000.0
 var swing = false
 
+
 func _physics_process(delta):
 	velocity  = Input.get_vector("a",'d','w','s') * SPEED * delta
 	print(swing)
+	
+	
 	if velocity == Vector2.ZERO && !swing:
 		$AnimationTree.get("parameters/playback").travel("idle")
 	
@@ -13,11 +16,13 @@ func _physics_process(delta):
 		$AnimationTree.get("parameters/playback").travel("walk")
 		$AnimationTree.set("parameters/idle/blend_position", velocity)
 		$AnimationTree.set("parameters/walk/blend_position", velocity)
-		$AnimationTree.set("parameters/swingPickaxe/blend_position", velocity)
+		
 		move_and_slide()
 	
 func _input(event):
 	if event.is_action_pressed("leftClick", false) && !swing && !global.bronze_pickaxe_drop:
+		$AnimationTree.set("parameters/swingPickaxe/blend_position", global_position.direction_to(get_global_mouse_position()))
+		$AnimationTree.set("parameters/idle/blend_position", global_position.direction_to(get_global_mouse_position()))
 		swing = true
 		$AnimationTree.get("parameters/playback").travel("swingPickaxe")
 		
