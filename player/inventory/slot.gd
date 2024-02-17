61f4a9743ad54344d3fe12fb3287ae9d4c007367
Nont Nonta <nontnta@gmail.com>
2024-02-18 01:15:45 +0700

    15

---
+++ player/inventory/slot.gd
@@ -0,0 +1,30 @@
+extends PanelContainer
+class_name Slot
+
+@onready var texture_rect = $slot_texture
+
+@export_enum("NONE:0","ARMOR:1","LENS:2","BOOTS:3","ACTIVE:4") var slot_type: int
+
+func _get_drag_data(at_position):
+	set_drag_preview(get_preview())
+	return texture_rect
+	
+func _can_drop_data(_pos, data):
+	return data is TextureRect
+
+func _drop_data(_pos, data):
+	var temp = texture_rect.property
+	texture_rect.property = data.property
+	data.property = temp
+
+func get_preview():
+	var preview_texture = TextureRect.new()
+	
+	preview_texture.texture = texture_rect.texture
+	preview_texture.expand_mode = 1
+	preview_texture.size = Vector2(30,30)
+	
+	var preview = Control.new()
+	preview.add_child(preview_texture) 
+	return preview
+	
