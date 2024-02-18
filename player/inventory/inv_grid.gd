cf0c0a9988f79b9cccea96c269531ffd9a290eab
Nont Nonta <nontnta@gmail.com>
2024-02-19 02:01:29 +0700

    j

--- player/inventory/inv_grid.gd
+++ player/inventory/inv_grid.gd
@@ -14,13 +14,23 @@ func add_item(ID = "0"):
 					"quantity": item_quantity}
 					
 	var index = 0
+	var first_slot = true
+	var first_index = 0
+	var no_dup = true
 	for i in get_children():
 		item_quantity = i.get_quantity()
+		if i.filled == false && first_slot == true:
+			first_index = i.get_index()
+			first_slot = false
 		if i.filled == true && item_ID == i.get_ID() :
 			index = i.get_index()
 			item_data["quantity"] = item_quantity+1
+			no_dup = false
 			break
-		elif i.filled == false:
-			index = i.get_index()
-			break
-	get_child(index).set_property(item_data)
+		#elif i.filled == false:
+			#index = i.get_index()
+		
+	if no_dup:
+		get_child(first_index).set_property(item_data)
+	if !no_dup:
+		get_child(index).set_property(item_data)
