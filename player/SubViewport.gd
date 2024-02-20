3c71b489341faa835bb139044a91c75d300dd475
Nont Nonta <nontnta@gmail.com>
2024-02-20 16:10:24 +0700

    23

--- player/SubViewport.gd
+++ player/SubViewport.gd
@@ -3,7 +3,10 @@ extends SubViewport
 @onready var camera = $Camera2D
 
 func _ready():
-	pass
+	$minimap_bg.position = $"../../../../bg".position
+	$minimap_bg.scale.x = $"../../../../bg".scale.x
+	$minimap_bg.scale.y = $"../../../../bg".scale.y
+	$minimap_rocket.position = $"../../../../spawn_player".position
 
 
 func _physics_process(_delta):
