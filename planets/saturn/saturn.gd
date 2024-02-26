10095044900654f7007d090e65d2f9eeaee29fa4
Nont Nonta <nontnta@gmail.com>
2024-02-26 22:10:40 +0700

    38

--- planets/saturn/saturn.gd
+++ planets/saturn/saturn.gd
@@ -2,18 +2,27 @@ extends Node2D
 
 @onready var current_pos = $spawn_player
 @export var player: PackedScene
+@export var green_critter: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Transition.takeOffArea = true
 	var player_spawn = player.instantiate()
 	player_spawn.position = current_pos.global_position
 	get_tree().current_scene.call_deferred('add_child', player_spawn)
-
+	greenCritter_spawn()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func greenCritter_spawn():
+	var i = 0
+	while  i < 5:
+		var critter_spawner = green_critter.instantiate()
+		critter_spawner.position = Vector2i(randi_range(700, 1300),randi_range(0,700))
+		get_tree().current_scene.call_deferred('add_child', critter_spawner)
+		i += 1
+		
 
 func _on_takeoff_area_entered(area):
 	if area.is_in_group('player'):
