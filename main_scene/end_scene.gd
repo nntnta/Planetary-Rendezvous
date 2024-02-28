20b2b399078da950f195dfa8e2cbf75b74024c72
Nont Nonta <nontnta@gmail.com>
2024-02-28 19:34:58 +0700

    49

--- main_scene/end_scene.gd
+++ main_scene/end_scene.gd
@@ -3,9 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if global.unique_all_planets:
+	if global.unique_all_planets && !global.catepillar_dead:
+		$butterfly.visible = false
 		$AnimationPlayer.play('good')
-
+	
+	if global.catepillar_dead:
+		$butterfly.visible = true
+		$good.visible = false
+		$AnimationPlayer.play('butterfly')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
